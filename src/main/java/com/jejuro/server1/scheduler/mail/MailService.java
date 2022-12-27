4cb63d56b1b63a8dc1658680bd0417d77ec7244d
sktkddn777 <sktkddn777@naver.com>
2022-12-27 16:07:39 +0900

    Update: MailService 현재 날짜 이전 알람 설정은 삭제

--- src/main/java/com/jejuro/server1/scheduler/mail/MailService.java
+++ src/main/java/com/jejuro/server1/scheduler/mail/MailService.java
@@ -2,57 +2,62 @@ package com.jejuro.server1.scheduler.mail;
 
 import com.jejuro.server1.entity.Alarm;
 import com.jejuro.server1.entity.Flight;
-import com.jejuro.server1.entity.Member;
 import com.jejuro.server1.repository.AlarmRepository;
 import com.jejuro.server1.repository.FlightRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class MailService {
 
     private final JavaMailSender javaMailSender;
     private final AlarmRepository alarmRepository;
     private final FlightRepository flightRepository;
 
-    @Scheduled(cron = "0 0 10 * * *")
+    @Scheduled(cron = "* * 10 * * *")
     public void mailSchedule() {
         // 알람 테이블에서 처리 안된 알람 구하기
         // 항공편 정보와 가격 비교
         List<String> mailList = getMailList();
-        sendMail(mailList);
+        //sendMail(mailList);
     }
 
     public List<String> getMailList() {
         List<Alarm> alarmList = alarmRepository.findByStatus(1);
         List<String> emailList = new ArrayList<>();
+        String now =  LocalDate.now().toString().replaceAll("-", "");
+
         for (Alarm alarm : alarmList) {
-            String code = alarm.getFlightCode();
-            String depDate = alarm.getDepDate();
-            int wantPrice = alarm.getPrice();
+            String code = alarm.getFlightCode(); // 항공편 고유 코드 번호
+            String depDate = alarm.getDepDate(); // 항공편 출발날짜
+            int wantPrice = alarm.getPrice(); // 사용자가 설정한 원하는 알람 가격
 
-            System.out.println(code);
-            System.out.println(depDate);
-            List<Flight> flights = flightRepository.findByFlightCodeAndDepDate(code, depDate);
-
-            Flight flight = flights.get(0);
-            System.out.println("flightId:"+flight.getId());
-            System.out.println("wantPrice:"+wantPrice);
-
-            if (wantPrice >= flight.getFee()) {
-                System.out.println("alarm.getMember():"+alarm.getMember());
-                String email = alarm.getMember().getEmail();
-                System.out.println("alarmId:"+alarm.getId());
+            // 만약 오늘 날짜가 항공편 출발날짜보다 크다면 지난 알림이므로 처리한거로 침
+            if (Integer.parseInt(now) > Integer.parseInt(depDate)) {
                 alarmRepository.setStatusDone(alarm.getId());
-                emailList.add(email);
-                System.out.println("email:"+email);
+            }
+            // 오늘 날짜보다 이전 알람이면 확인
+            else {
+                List<Flight> flights = flightRepository.findByFlightCodeAndDepDate(code, depDate);
+
+                if (flights.size()>0) {
+                    Flight flight = flights.get(0);
+                    if (wantPrice >= flight.getFee()) {
+                        String email = alarm.getMember().getEmail();
+                        alarmRepository.setStatusDone(alarm.getId());
+                        emailList.add(email);
+                    }
+                }
             }
         }
         return emailList;
