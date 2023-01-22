ea3d818370388d7b18e0f82ce11459fd2243b064
Jungwoo <cloudwhale99@gmail.com>
2023-01-22 23:43:18 +0900

    Feat: Sending email in Spring with Thymeleaf
    기존에 이메일 보내기 기능에서 단순 메세지가 아닌 템플릿과 context 정보를 랜더링해서 보내준다.
    그리고 알람을 보내야하는 이메일 정보를 가지고 오는 getMailList를 context에 들어갈 정보를 가져오기 위해
    getIdList로 바꾸고 알람 id 정보들을 반환한다.
    메일 제목, 발신자, 내용을 설정하고 메일을 스케줄 시간에 맞추어 보낸다.

--- src/main/java/com/jejuro/server1/repository/AlarmRepository.java
+++ src/main/java/com/jejuro/server1/repository/AlarmRepository.java
@@ -9,12 +9,15 @@ import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Transactional
 @Repository
 public interface AlarmRepository extends JpaRepository<Alarm, Long> {
 
     List<Alarm> findByStatus(int status);
+
+    Optional<Alarm> findById(Long id);
 
     @Modifying
     @Query("update Alarm a set a.status = 0 where a.id = :id")
--- src/main/java/com/jejuro/server1/scheduler/mail/MailService.java
+++ src/main/java/com/jejuro/server1/scheduler/mail/MailService.java
@@ -2,18 +2,30 @@ package com.jejuro.server1.scheduler.mail;
 
 import com.jejuro.server1.entity.Alarm;
 import com.jejuro.server1.entity.Flight;
+import com.jejuro.server1.entity.Member;
 import com.jejuro.server1.repository.AlarmRepository;
 import com.jejuro.server1.repository.FlightRepository;
+
+import jakarta.mail.MessagingException;
+import jakarta.mail.internet.MimeMessage;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.mail.SimpleMailMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
-
+import org.thymeleaf.TemplateEngine;
+import org.thymeleaf.context.Context;
+import java.io.UnsupportedEncodingException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Optional;
 
 @Component
 @RequiredArgsConstructor
@@ -24,20 +36,70 @@ public class MailService {
     private final AlarmRepository alarmRepository;
     private final FlightRepository flightRepository;
 
-//    이메일 알림 시간 설정
-    @Scheduled(cron = "0 32 16 * * *")
-    public void mailSchedule() {
-        // 알람 테이블에서 처리 안된 알람 구하기
-        // 항공편 정보와 가격 비교
-        List<String> mailList = getMailList();
-        for(String mail : mailList)
-            sendMail(mail);
+    @Autowired
+    private TemplateEngine templateEngine;
+
+    /**
+     * JavaMailSender API를 통해 알람을 메일로 보내는 메소드다.
+     * id를 통해 메일을 송신하기 때문에 알람을 다중으로 설정하려면 추후 기능 추가 필요
+     */
+    @Scheduled(cron = "* * * * * *")
+    public void mailSchedule() throws MessagingException, UnsupportedEncodingException, ParseException {
+
+        MimeMessage message = javaMailSender.createMimeMessage();
+        MimeMessageHelper helper = new MimeMessageHelper(message, true, "UTF-8");
+
+        // getIdList함수를 통해 메일을 보낼 id를 가져온다.
+        List<Long> idList = getIdList();
+
+        for (Long id : idList) {
+
+            Optional<Alarm> alarm = alarmRepository.findById(id);
+            Member member = alarm.get().getMember();
+
+            // 날짜 포맷을 변경
+            SimpleDateFormat dtFormat = new SimpleDateFormat("yyyyMMdd");
+            SimpleDateFormat newDtFormat = new SimpleDateFormat("yyy-MM-dd");
+
+            Date formDate = dtFormat.parse(alarm.get().getDepDate());
+
+            // 항공편 출발날짜
+            String depDate = newDtFormat.format(formDate);
+            // 항공편 고유 코드 번호
+            String code = alarm.get().getFlightCode();
+            // 사용자가 설정한 원하는 알람 가격
+            int price = alarm.get().getPrice();
+            // 출력을 위한 사용자 닉네임
+            String nickname = member.getNickName();
+            // 사용자 이메일
+            String email = member.getEmail();
+            // 메일 제목 설정
+            helper.setSubject("[JEJURO] 알림하신 가격대가 도착했어요");
+            // 메일 발신자 설정
+            helper.setFrom("Info@Jejuro.com", "Jejuro");
+
+            // 랜더링을 하기 위해 필요한 정보들
+            Context context = new Context();
+            context.setVariable("price", price);
+            context.setVariable("depDate", depDate);
+            context.setVariable("flightCode", code);
+            context.setVariable("name", nickname);
+
+            // 메일 내용 설정 : html페이지와 context객체 렌더링 작업
+            String html = templateEngine.process("index.html", context);
+
+            helper.setText(html, true);
+            helper.setTo(email);
+
+            // 메일 보내기
+            javaMailSender.send(message);
+        }
     }
 
-    public List<String> getMailList() {
+    public List<Long> getIdList() {
         List<Alarm> alarmList = alarmRepository.findByStatus(1);
-        List<String> emailList = new ArrayList<>();
-        String now =  LocalDate.now().toString().replaceAll("-", "");
+        List<Long> idList = new ArrayList<>();
+        String now = LocalDate.now().toString().replaceAll("-", "");
 
         for (Alarm alarm : alarmList) {
             String code = alarm.getFlightCode(); // 항공편 고유 코드 번호
@@ -50,27 +112,18 @@ public class MailService {
             // 오늘 날짜보다 이전 알람이면 확인
             else {
                 List<Flight> flights = flightRepository.findByFlightCodeAndDepDate(code, depDate);
-                System.out.println("flightSize:"+flights.size());
-                if (flights.size()>0) {
+                System.out.println("flightSize:" + flights.size());
+                if (flights.size() > 0) {
                     Flight flight = flights.get(0);
                     if (wantPrice >= flight.getFee()) {
-                        String email = alarm.getMember().getEmail();
+                        Long id = alarm.getId();
                         alarmRepository.setStatusDone(alarm.getId());
-                        emailList.add(email);
+                        idList.add(id);
                     }
                 }
             }
         }
-        return emailList;
+        return idList;
     }
 
-    public void sendMail(String mail) {
-        SimpleMailMessage message = new SimpleMailMessage();
-
-        message.setTo(mail);
-        message.setSubject("[JEJURO] 알림하신 가격대가 도착했어요");
-        message.setText(" http://localhost:8081 에서 가격을 확인해 보세요");
-
-        javaMailSender.send(message);
-    }
 }
